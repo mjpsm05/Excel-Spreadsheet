ed994a6ce97ef72cb65f3d9674234e84ddc68315
mjpsm05 <mebamazamesso@gmail.com>
2024-06-19 12:36:54 -0400

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,8 +1,7 @@
 import streamlit as st 
 import pandas as pd  
 from PIL import Image
-import matplotlib.pyplot as plt
-import numpy as np
+
 
 st.title("Excel Spreadsheet Data")
 st.header("By Mazamesso Meba")
